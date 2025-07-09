0554e177e47b49245651c77a8e642d9c29a62df7
Christan <ccavrey@gmail.com>
2025-07-09 21:54:58 +0200

    Refactoring config

--- configuration.nix
+++ configuration.nix
@@ -1,34 +1,24 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
+  system.stateVersion = "25.05";
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnsupportedSystem = false;
+
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/nvme0n1";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Africa/Johannesburg";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
 
   fonts.packages = with pkgs; [
@@ -43,8 +33,12 @@
       ];
     };
   };
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+
+  # Keymap
+  services.xserver.xkb = {
+    layout = "za";
+    variant = "";
+  };
 
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
@@ -54,19 +48,17 @@
   # services.displayManager.sddm.enable = true;
   # services.desktopManager.plasma6.enable = true;
 
-  programs.sway.enable = false;
+  services.xserver.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "za";
-    variant = "";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "christan";
+
+  # Sound
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -74,18 +66,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     # jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.christan = {
     isNormalUser = true;
     description = "christan";
@@ -98,8 +81,6 @@
       "input"
     ];
     shell = pkgs.zsh;
-    # packages = with pkgs; [
-    # ];
   };
 
   security.sudo.wheelNeedsPassword = true;
@@ -112,132 +93,59 @@
     '';
   };
 
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "christan";
-
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
-  # Install firefox.
   programs.firefox.enable = true;
+  programs.sway.enable = false;
 
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
 
+  programs.dconf.profiles.user.databases = [
+    {
+      settings."org/gnome/desktop/interface" = {
+        gtk-theme = "Adwaita";
+        icon-theme = "Flat-Remix-Red-Dark";
+        font-name = "Noto Sans Medium 11";
+        document-font-name = "Noto Sans Medium 11";
+        monospace-font-name = "Noto Sans Mono Medium 11";
+      };
+    }
+  ];
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      theme = "agnoster";
+      plugins = [
+        "git"
+        "z"
+        "history"
+        "sudo"
+        "docker"
+        "docker-compose"
+        "aws"
+        "azure"
+        "argocd"
+        "kubectl"
+        "kubectx"
+        "pip"
+        "ssh"
+        "terraform"
+      ];
+    };
+  };
+
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
   };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnsupportedSystem = false;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-    brave
-    chromium
-    vim
-    # Core Packages
-    neovim
-    gnumake
-    busybox
-    wget
-    stern
-    jq
-    yq
-    kubernetes-helm
-    openssl
-    go-task
-    virtualenv
-    kubectl
-    kubectx
-    kubelogin
-    git
-    postgresql
-    eksctl
-    lazygit
-    fd
-    ripgrep
-    chromium
-    flyctl
-    sops
-    gnupg
-    k9s
-    ssm-session-manager-plugin
-    azure-cli
-    awscli2
-    docker_26
-    docker-compose
-
-    # LSP's for neovim
-    terraform-ls
-    terraform-lsp
-    tflint
-    yaml-language-server
-    ansible-language-server
-    ansible-lint
-    lua-language-server
-    nodePackages.typescript-language-server
-    nodePackages.bash-language-server
-    jdt-language-server
-    postgres-lsp
-
-    dockerfile-language-server-nodejs
-    pyright
-    gopls
-    nodePackages.typescript-language-server
-    helm-ls
-    nixd
-
-    # Development
-    terraform
-    terragrunt
-    python312Full
-    python312Packages.ansible-core
-    go
-    nodejs_22
-    typescript
-    lua
-    yarn
-    k3s
-    minikube
-    jdk23
-    nixfmt-rfc-style
-
-    # Other
-    starship
-    zsh
-    glow
-    nvidia-container-toolkit
-    steampipe
-    tmux
-    btop
-    fzf
-    plantuml
-    graphviz
-
-    # hyprland
-    waybar
-    (pkgs.waybar.overrideAttrs (oldAttrs: {
-      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-    }))
-    dunst
-    libnotify
-    kitty
-    swww
-    #alacritty
-    rofi-wayland
-    networkmanagerapplet
-    wofi
-  ];
 
   environment.variables.EDITOR = "nvim";
   environment.etc."zshrc".text = ''
@@ -278,36 +186,9 @@
     EOF
   '';
 
-  programs.zsh = {
-    enable = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    ohMyZsh = {
-      enable = true;
-      theme = "agnoster";
-      plugins = [
-        "git"
-        "z"
-        "history"
-        "sudo"
-        "docker"
-        "docker-compose"
-        "aws"
-        "azure"
-        "argocd"
-        "kubectl"
-        "kubectx"
-        "pip"
-        "ssh"
-        "terraform"
-      ];
-    };
-  };
-
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-  # Docker
   virtualisation.docker = {
     enable = true;
     daemon.settings.features.cdi = true;
@@ -339,31 +220,100 @@
     };
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  environment.systemPackages = with pkgs; [
+    # Desktop apps
+    brave
+    chromium
 
-  # List services that you want to enable:
+    # Core Packages
+    neovim
+    gnumake
+    busybox
+    wget
+    stern
+    jq
+    yq
+    kubernetes-helm
+    openssl
+    go-task
+    virtualenv
+    kubectl
+    kubectx
+    kubelogin
+    git
+    postgresql
+    eksctl
+    lazygit
+    fd
+    ripgrep
+    chromium
+    flyctl
+    sops
+    gnupg
+    k9s
+    ssm-session-manager-plugin
+    azure-cli
+    awscli2
+    docker_26
+    docker-compose
+    zsh
+    steampipe
+    fzf
+    starship
+    glow
+    nvidia-container-toolkit
+    btop
+    plantuml
+    graphviz
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+    # LSP's for neovim
+    terraform-ls
+    terraform-lsp
+    tflint
+    yaml-language-server
+    ansible-language-server
+    ansible-lint
+    lua-language-server
+    nodePackages.typescript-language-server
+    nodePackages.bash-language-server
+    jdt-language-server
+    postgres-lsp
+    dockerfile-language-server-nodejs
+    pyright
+    gopls
+    nodePackages.typescript-language-server
+    helm-ls
+    nixd
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = true;
+    # Development
+    terraform
+    terragrunt
+    python312Full
+    python312Packages.ansible-core
+    go
+    nodejs_22
+    typescript
+    lua
+    yarn
+    k3s
+    minikube
+    jdk23
+    nixfmt-rfc-style
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+    # Terminals
+    wezterm
+    kitty
 
+    # hyprland
+    waybar
+    (pkgs.waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    }))
+    dunst
+    libnotify
+    swww
+    rofi-wayland
+    networkmanagerapplet
+    wofi
+  ];
 }
